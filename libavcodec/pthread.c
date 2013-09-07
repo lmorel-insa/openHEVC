fe70a2fdf00319178ab847263bee94413e18bba7
wassim Hamidouche <whamidouch@gmail.com>
2013-09-07 22:24:18 +0200

    cleanup

--- libavcodec/pthread.c
+++ libavcodec/pthread.c
@@ -1078,7 +1078,6 @@ void ff_thread_report_progress2(AVCodecContext *avctx, int field, int n)
 {
     ThreadContext *p  = avctx->thread_opaque;
     int *count_entries = p->count_entries;
-    if (field == (p->count-1)) return;
   
     pthread_mutex_lock(&p->progress_mutex[field]);
     count_entries[field] +=n;
