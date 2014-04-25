2ad701ac79f0a4d55cae1de93fc926e6461be878
wassim Hamidouche <whamidouch@gmail.com>
2014-04-25 12:02:55 +0200

    encrypt SAO parameters

--- libavcodec/hevc.c
+++ libavcodec/hevc.c
@@ -1078,6 +1078,119 @@ do {                                                    \
         sao->elem = 0;                                  \
 } while (0)
 
+#if EncryptSAOPars
+void swap_array(int * offset_abs, unsigned int *key){
+    int u, v, dx, dy, z1, z2, z3, i, j;
+    int offset_abs_p[4]; 
+    u  = (*key)&0x00000001;
+    *key = (*key) >> 1;
+    v  = (*key)&0x00000001;
+    *key = (*key) >> 1;
+    dx = (*key)&0x00000001;
+    *key = (*key) >> 1;
+    dy = dx ? ((*key)&0x00000001):1;
+    *key = (*key) >> 1;
+
+    z1=dx+dy;
+    z2=u*v+1;
+    i=0;j=0;
+    z3=v*i+dy;
+    offset_abs_p[((i+u*j+z1)&1)* 2 + ((z3+z2*j)&1)] = offset_abs[0];
+    i=0;j=1;
+    z3=v*i+dy;
+    offset_abs_p[((i+u*j+z1)&1)*2+((z3+z2*j)&1)] = offset_abs[1];
+    i=1;j=0;
+    z3=v*i+dy;
+    offset_abs_p[((i+u*j+z1)&1)*2+((z3+z2*j)&1)] = offset_abs[2];
+    i=1;j=1;
+    z3=v*i+dy;
+    offset_abs_p[((i+u*j+z1)&1)*2+((z3+z2*j)&1)] = offset_abs[3];
+
+    offset_abs[0] = offset_abs_p[0];
+    offset_abs[1] = offset_abs_p[1];
+    offset_abs[2] = offset_abs_p[2];
+    offset_abs[3] = offset_abs_p[3];
+}
+static void hls_sao_param(HEVCContext *s, int rx, int ry)
+{
+    HEVCLocalContext *lc    = s->HEVClc;
+    int sao_merge_left_flag = 0;
+    int sao_merge_up_flag   = 0;
+    int shift               = s->sps->bit_depth - FFMIN(s->sps->bit_depth, 10);
+    SAOParams *sao          = &CTB(s->sao, rx, ry);
+    int c_idx, i;
+    srand (0);
+    for(i=0; i< 64; i++) {
+        s->keybits = s->keybits << 1;
+        s->keybits = s->keybits | (unsigned int)(rand()%2);
+    }
+    
+    if (s->sh.slice_sample_adaptive_offset_flag[0] ||
+        s->sh.slice_sample_adaptive_offset_flag[1]) {
+        if (rx > 0) {
+            if (lc->ctb_left_flag)
+                sao_merge_left_flag = ff_hevc_sao_merge_flag_decode(s);
+        }
+        if (ry > 0 && !sao_merge_left_flag) {
+            if (lc->ctb_up_flag)
+                sao_merge_up_flag = ff_hevc_sao_merge_flag_decode(s);
+        }
+    }
+    for (c_idx = 0; c_idx < 3; c_idx++) {
+        if (!s->sh.slice_sample_adaptive_offset_flag[c_idx]) {
+            sao->type_idx[c_idx] = SAO_NOT_APPLIED;
+            continue;
+        }
+        if (c_idx == 2) {
+            sao->type_idx[2] = sao->type_idx[1];
+            sao->eo_class[2] = sao->eo_class[1];
+        } else {
+            SET_SAO(type_idx[c_idx], ff_hevc_sao_type_idx_decode(s));
+            }
+
+        if (sao->type_idx[c_idx] == SAO_NOT_APPLIED)
+            continue;
+
+        for (i = 0; i < 4; i++)
+            SET_SAO(offset_abs[c_idx][i], ff_hevc_sao_offset_abs_decode(s));
+
+        if(sao->type_idx[c_idx] == SAO_EDGE && !sao_merge_left_flag && !sao_merge_up_flag) {
+          /*  for (i = 0; i < 4; i++)
+                printf("%d  ", sao->offset_abs[c_idx][i]);*/
+            swap_array( sao->offset_abs[c_idx], &s->keybits );
+          /*  for (i = 0; i < 4; i++)
+                printf("%d  ", sao->offset_abs[c_idx][i]);
+                        printf("%d \n", c_idx);*/
+        }
+        if (sao->type_idx[c_idx] == SAO_BAND) {
+            for (i = 0; i < 4; i++) {
+                if (sao->offset_abs[c_idx][i]) {
+                    SET_SAO(offset_sign[c_idx][i],
+                            ff_hevc_sao_offset_sign_decode(s));
+                } else {
+                    sao->offset_sign[c_idx][i] = 0;
+                }
+            }
+            SET_SAO(band_position[c_idx], ff_hevc_sao_band_position_decode(s));
+            //printf("pos: %d  \n", sao->band_position[c_idx]);
+        } else if (c_idx != 2) {
+            SET_SAO(eo_class[c_idx], ff_hevc_sao_eo_class_decode(s));
+        }
+
+        // Inferred parameters
+        sao->offset_val[c_idx][0] = 0;
+        for (i = 0; i < 4; i++) {
+            sao->offset_val[c_idx][i + 1] = sao->offset_abs[c_idx][i] << shift;
+            if (sao->type_idx[c_idx] == SAO_EDGE) {
+                if (i > 1)
+                    sao->offset_val[c_idx][i + 1] = -sao->offset_val[c_idx][i + 1];
+            } else if (sao->offset_sign[c_idx][i]) {
+                sao->offset_val[c_idx][i + 1] = -sao->offset_val[c_idx][i + 1];
+            }
+        }
+    }
+}
+#else
 static void hls_sao_param(HEVCContext *s, int rx, int ry)
 {
     HEVCLocalContext *lc    = s->HEVClc;
@@ -1145,7 +1258,7 @@ static void hls_sao_param(HEVCContext *s, int rx, int ry)
         }
     }
 }
-
+#endif
 #undef SET_SAO
 #undef CTB
 
