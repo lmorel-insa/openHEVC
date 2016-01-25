de773615867d91bfe1492ac42415f4cd8d33178f
lmorel-insa <lionel.morel@inria.fr>
2016-01-25 15:08:27 +0100

    ...

--- main_hm/main.c
+++ main_hm/main.c
@@ -532,7 +532,7 @@ int main(int argc, char *argv[]) {
   for (i = 0; i < nb_pthreads; i++) {
 	//Start memory access sampling
 	numap_rc = numap_sampling_read_start(&measures[i]);
-	if(res < 0) {
+	if(numap_rc < 0) {
 	  fprintf(stderr, "numap_sampling_start error : %s\n", numap_error_message(res));
 	  pthread_exit(NULL);
 	} else {
